b8c6b7e86ce69ad610b297785f39e3c22fb3f29a
fkffkfk <minisnake@naver.com>
2023-04-05 15:05:22 +0900

    0405_웹응용수업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import Todo from './Todo';
 import './App.css';
 
+/*class App extends React.Component{
+  render(){
+    return(
+      <div className="App">
+        <Todo />
+        <Todo />
+      </div>
+    );
+  }
+}*/
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      item: {id: 0, title: "Hello world 1", done: true}};
+    }
+  
+  render() {
+    return(
+    <div className="App">
+      <Todo item={this.state.item} />
+  
+    </div>
+    );
+  }
+}
+/*
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todo />
     </div>
   );
-}
+}*/
+
 
 export default App;
