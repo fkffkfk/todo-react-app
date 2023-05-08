dcef2d9090a182a9e47b6f19b1885d3efe29b229
fkffkfk <minisnake@naver.com>
2023-05-08 16:18:29 +0900

    5/8 수업종료

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+class Login extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+render(){
+    return <p>로그인 페이지</p>;
+}
+}
+export default Login;
